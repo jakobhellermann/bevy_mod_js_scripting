c0776e3d427a72969623160ad5edbc7d4f07ce71
Zicklag <zicklag@katharostech.com>
2022-08-13 21:31:45 -0500

    Implement World Info Bindings For the Browser

--- src/runtime/wasm/wasm_setup.js
+++ src/runtime/wasm/wasm_setup.js
@@ -4,8 +4,23 @@ export function setup_js_globals(bevyModJsScripting) {
 }
 
 function bevyModJsScriptingOpSync(op_name, ...args) {
+    const WORLD_RID = 0;
     switch (op_name) {
         case "op_log":
-            window.bevyModJsScripting.op_log(args[0], JSON.stringify(args[1]));
+            return window.bevyModJsScripting.op_log(
+                args[0],
+                JSON.stringify(args[1])
+            );
+        case "op_world_tostring":
+            return window.bevyModJsScripting.op_world_tostring(WORLD_RID);
+        case "op_world_components":
+            return window.bevyModJsScripting.op_world_components(WORLD_RID);
+        case "op_world_resources":
+            return window.bevyModJsScripting.op_world_resources(WORLD_RID);
+        case "op_world_entities":
+            return window.bevyModJsScripting.op_world_entities(WORLD_RID);
+        default:
+            console.error(`Op not implemented for browser yet: ${op_name}`);
+            return;
     }
 }
