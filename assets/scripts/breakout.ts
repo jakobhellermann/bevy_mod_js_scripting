d352c4f1c6e068ac40c74df722dd480f5a481022
Jakob Hellermann <jakob.hellermann@protonmail.com>
2022-11-16 18:29:31 +0100

    remove notaballyet

--- assets/scripts/breakout.ts
+++ assets/scripts/breakout.ts
@@ -11,10 +11,6 @@ type Velocity = {
 };
 const Velocity: BevyType<Velocity> = { typeName: "breakout::Velocity" };
 
-type NotABallYet = unknown;
-const NotABallYet: BevyType<NotABallYet> = {
-  typeName: "breakout::NotABallYet",
-};
 type Ball = unknown;
 const Ball: BevyType<Ball> = { typeName: "breakout::Ball" };
 
@@ -26,41 +22,12 @@ function run() {
     info(score.score);
   }
 
-  // Start the ball movement
-  if (i == 60) {
-    // Query the entity that has the NotABallYet component
-    for (const item of world.query(NotABallYet)) {
-      // Insert the ball component on that entity
-      world.insert(item.entity, Value.create(Ball));
-
-      // Create a velocity component
-      //
-      // We can optionally include a patch to the default value of velocity as the second argument
-      // to create().
-      let vel = Value.create(Velocity, [
-        {
-          x: -200,
-        },
-      ]);
-
-      // For demonstration purposes, we can also patch values after they have been created. This
-      // works on any ECS component, not just ones created with Value.create().
-      Value.patch(vel, [
-        {
-          y: 200,
-        },
-      ]);
-
-      // Add the velocity to the ball
-      world.insert(item.entity, vel);
-    }
-  }
-
   if (firstIteration) {
     firstIteration = false;
 
     for (const item of world.query(Transform, Velocity)) {
       let [transform, velocity] = item.components;
+
       info("Velocity:", velocity[0].toString());
       info("Transform:", transform.translation.toString());
     }
